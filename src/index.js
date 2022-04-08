36dc9fe64e90b89818a8e36413a12e425a3cd553
HordiienkoOlha <91278187+HordiienkoOlha@users.noreply.github.com>
2022-04-08 21:42:38 +0300

    edit css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
+// Notify.info('Too many matches found. Please enter a more specific name.');
+// Notify.failure('Oops, there is no country with that name');
+
+const textInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+// // console.log(textInput);
+// // console.log(countryList);
+// // console.log(countryInfo);
+textInput.addEventListener('input', debounce(onClick, 300));
+function onClick() {
+  console.log(textInput.value);
+}
+
+// countryList.textContent = event.currentTarget.value;
