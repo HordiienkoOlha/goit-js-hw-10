ea16d61e01cc1ec29912a457b96df393fcf55361
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-04-11 18:40:41 +0300

    add form and card html

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import debounce from 'lodash.debounce';
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 const textInput = document.querySelector('#search-box');
-
+const formRef = document.querySelector('.js-search-form')
+console.log(formRef)
 // Notify.info('Too many matches found. Please enter a more specific name.');
 // Notify.failure('Oops, there is no country with that name');
 
@@ -15,7 +16,7 @@ textInput.addEventListener('input', debounce(onClick, 300));
 // textInput.addEventListener('input', onClick());
 
 function onClick() {
-  const name = textInput.value;
+  const name = textInput.value.trim();
   fetchCountries(name)
     .then(data => renderCountryList(data))
     .catch(error => {
